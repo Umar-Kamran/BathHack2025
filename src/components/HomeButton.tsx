32604f2543386f6834591eb0daa4c75a48caf6e7
n33vb4k <bakshi.neev@gmail.com>
2025-04-06 02:56:14 +0100

    fix spacing

--- src/components/HomeButton.tsx
+++ src/components/HomeButton.tsx
@@ -6,7 +6,7 @@ type buttonProps = {
 const Button = ({ text, onClick }: buttonProps) => {
   return (
     <button
-      className="w-full bg-[var(--color-secondary)] text-[var(--color-primary)] hover:text-[var(--color-tertiary)] text-3xl py-3 px-20 rounded duration-300 shadow-lg shadow-black text-center"
+      className="w-full h-full bg-[var(--color-secondary)] text-[var(--color-primary)] hover:text-[var(--color-tertiary)] text-3xl py-3 px-20 rounded duration-300 shadow-lg shadow-black text-center"
       onClick={onClick}
     >
       {text}
