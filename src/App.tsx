2033112d17849ee0a7dd24d1e2b5a2d2537de98e
n33vb4k <bakshi.neev@gmail.com>
2025-04-06 01:20:14 +0100

    fix import

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import Home from './pages/home'
+import Home from './pages/Home'
 import { ToastContainer } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
 import GoogleLogin from './components/GoogleLogin'
